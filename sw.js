4e2c42db176eb5b7d67bdc87fe72237dc7bc3c17
Dvoynikov Kirill <illdv@mail.ru>
2018-06-22 23:02:47 +0300

    sw1

--- sw.js
+++ sw.js
@@ -1,11 +1,32 @@
+const CACHE = 'cache-and-update-v1';
+
+// При установке воркера мы должны закешировать часть данных (статику).
 self.addEventListener('install', (event) => {
-	console.log('Установлен');
+    event.waitUntil(
+        caches.open(CACHE).then((cache) =>
+            cache.addAll(['img/first-back.jpg']))
+    );
 });
 
-self.addEventListener('activate', (event) => {
-	console.log('Активирован');
+// при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
+self.addEventListener('fetch', function(event) {
+    // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
+    event.respondWith(fromCache(event.request));
+    // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
+    event.waitUntil(update(event.request));
 });
 
-self.addEventListener('fetch', (event) => {
-	console.log('Происходит запрос на сервер');
-});
+function fromCache(request) {
+    return caches.open(CACHE).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
+}
+
+function update(request) {
+    return caches.open(CACHE).then((cache) =>
+        fetch(request).then((response) =>
+            cache.put(request, response)
+        )
+    );
+}
