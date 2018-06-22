652e5b7f366914faa001ae853d3176070718dd71
Dvoynikov Kirill <illdv@mail.ru>
2018-06-22 23:31:38 +0300

    a

--- sw.js
+++ sw.js
@@ -1,24 +1,40 @@
-const CACHE = 'cache-only-v1';
-
+const CACHE = 'network-or-cache-v1';
+const timeout = 400;
 // При установке воркера мы должны закешировать часть данных (статику).
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE).then((cache) => {
-            return cache.addAll([
+        caches.open(CACHE).then((cache) => cache.addAll([
                 '/img/first-back.jpg'
-            ]);
-        })
-    );
+            ])
+        ));
 });
 
-// При запросе на сервер (событие fetch), используем только данные из кэша.
-self.addEventListener('fetch', (event) =>
-    event.respondWith(fromCache(event.request))
-);
+// при событии fetch, мы и делаем запрос, но используем кэш, только после истечения timeout.
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fromNetwork(event.request, timeout)
+      .catch((err) => {
+          console.log(`Error: ${err.message()}`);
+          return fromCache(event.request);
+      }));
+});
+
+// Временно-ограниченный запрос.
+function fromNetwork(request, timeout) {
+    return new Promise((fulfill, reject) => {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then((response) => {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+}
 
 function fromCache(request) {
+// Открываем наше хранилище кэша (CacheStorage API), выполняем поиск запрошенного ресурса.
+// Обратите внимание, что в случае отсутствия соответствия значения Promise выполнится успешно, но со значением `undefined`
     return caches.open(CACHE).then((cache) =>
-      cache.match(request)
-          .then((matching) => matching || Promise.reject('no-match'))
-    );
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
 }
+console.log('aaaaaaaaaaaaaaaa');
