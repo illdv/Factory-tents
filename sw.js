8c3da076a450d446dbb99ba6d41668250e3f02c0
Dvoynikov Kirill <illdv@mail.ru>
2018-06-22 22:52:28 +0300

    sw

--- sw.js
+++ sw.js
@@ -1,32 +1,11 @@
-// const CACHE = 'cache-and-update-v1';
+self.addEventListener('install', (event) => {
+	console.log('Установлен');
+});
 
-// // При установке воркера мы должны закешировать часть данных (статику).
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE).then((cache) =>
-//             cache.addAll(['img' , 'fonts', 'css']))
-//     );
-// });
+self.addEventListener('activate', (event) => {
+	console.log('Активирован');
+});
 
-// // при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
-// self.addEventListener('fetch', function(event) {
-//     // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
-//     event.respondWith(fromCache(event.request));
-//     // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
-//     event.waitUntil(update(event.request));
-// });
-
-// function fromCache(request) {
-//     return caches.open(CACHE).then((cache) =>
-//         cache.match(request).then((matching) =>
-//             matching || Promise.reject('no-match')
-//         ));
-// }
-
-// function update(request) {
-//     return caches.open(CACHE).then((cache) =>
-//         fetch(request).then((response) =>
-//             cache.put(request, response)
-//         )
-//     );
-// }
+self.addEventListener('fetch', (event) => {
+	console.log('Происходит запрос на сервер');
+});
