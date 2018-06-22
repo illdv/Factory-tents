b4d1c254b39de3ddcfc65a050ef1eb10ee46bfa2
Dvoynikov Kirill <illdv@mail.ru>
2018-06-22 18:33:18 +0300

    added logo

--- js/sw.js
+++ js/sw.js
@@ -1,32 +1,3 @@
-// const CACHE = 'cache-and-update-v1';
 
-// // При установке воркера мы должны закешировать часть данных (статику).
-// self.addEventListener('install', (event) => {
-// 	event.waitUntil(
-// 		caches.open(CACHE).then((cache) =>
-// 			cache.addAll(['../img', '../fonts', '../css']))
-// 	);
-// });
-
-// // при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
-// self.addEventListener('fetch', function (event) {
-// 	// Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
-// 	event.respondWith(fromCache(event.request));
-// 	// `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
-// 	event.waitUntil(update(event.request));
-// });
-
-// function fromCache(request) {
-// 	return caches.open(CACHE).then((cache) =>
-// 		cache.match(request).then((matching) =>
-// 			matching || Promise.reject('no-match')
-// 		));
-// }
-
-// function update(request) {
-// 	return caches.open(CACHE).then((cache) =>
-// 		fetch(request).then((response) =>
-// 			cache.put(request, response)
-// 		)
-// 	);
-// }
+const CACHE='cache-and-update-v1';self.addEventListener('install',(event)=>{event.waitUntil(caches.open(CACHE).then((cache)=>cache.addAll(['../img/*','../fonts/*','../css/*','../js/*','../index.html'])));});self.addEventListener('fetch',function(event){event.respondWith(fromCache(event.request));event.waitUntil(update(event.request));});function fromCache(request){return caches.open(CACHE).then((cache)=>cache.match(request).then((matching)=>matching||Promise.reject('no-match')));}
+function update(request){return caches.open(CACHE).then((cache)=>fetch(request).then((response)=>cache.put(request,response)));}
